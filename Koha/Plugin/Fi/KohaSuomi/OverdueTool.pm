b6446d7e4c58f4a0d59f3c53d364dae1c5d360b1
Johanna Raisa <johanna.raisa@gmail.com>
2022-01-10 13:22:01 +0200

    Change name on button

--- Koha/Plugin/Fi/KohaSuomi/OverdueTool.pm
+++ Koha/Plugin/Fi/KohaSuomi/OverdueTool.pm
@@ -14,14 +14,14 @@ use JSON;
 use Koha::Plugin::Fi::KohaSuomi::OverdueTool::Modules::Config;
 
 ## Here we set our plugin version
-our $VERSION = "1.5.7";
+our $VERSION = "1.5.8";
 
 ## Here is our metadata, some keys are required, some are optional
 our $metadata = {
     name            => 'Laskutustyökalu',
     author          => 'Johanna Räisä',
     date_authored   => '2020-12-28',
-    date_updated    => "2022-01-10",
+    date_updated    => "2022-01-11",
     minimum_version => '17.05.00.000',
     maximum_version => undef,
     version         => $VERSION,

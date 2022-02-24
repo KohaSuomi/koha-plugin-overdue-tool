33e6b97224c110cd9f543746dc54a7591bc324dd
Johanna Raisa <johanna.raisa@gmail.com>
2022-02-24 12:51:11 +0200

    Add new table values to set-development

--- set-development.sh
+++ set-development.sh
@@ -17,9 +17,14 @@ PASS=`xmlstarlet sel -t -v 'yazgfs/config/pass' $KOHA_CONF`
 mysql --user=$USER --password="$PASS" --port=$PORT --host=$HOST $DATABASE << END
 DELETE FROM plugin_data where plugin_class = 'Koha::Plugin::Fi::KohaSuomi::OverdueTool';
 DELETE FROM plugin_methods where plugin_class = 'Koha::Plugin::Fi::KohaSuomi::OverdueTool';
+DELETE FROM message_transport_types where message_transport_type = 'finvoice';
+DELETE FROM message_transport_types where message_transport_type = 'pdf';
+INSERT INTO message_transport_types (message_transport_type) VALUES ('finvoice');
+INSERT INTO message_transport_types (message_transport_type) VALUES ('pdf');
 INSERT INTO plugin_data (plugin_class,plugin_key,plugin_value) VALUES ('Koha::Plugin::Fi::KohaSuomi::OverdueTool','__INSTALLED__','1');
 INSERT INTO plugin_data (plugin_class,plugin_key,plugin_value) VALUES ('Koha::Plugin::Fi::KohaSuomi::OverdueTool','__INSTALLED_VERSION__','${VERSION}');
 INSERT INTO plugin_data (plugin_class,plugin_key,plugin_value) VALUES ('Koha::Plugin::Fi::KohaSuomi::OverdueTool','__ENABLED__','1');
+INSERT INTO plugin_data (plugin_class,plugin_key,plugin_value) VALUES ('Koha::Plugin::Fi::KohaSuomi::OverdueTool','invoicenumber','1');
 
 INSERT INTO plugin_methods (plugin_class, plugin_method) values 
      ('Koha::Plugin::Fi::KohaSuomi::OverdueTool', 'abs_path'),
@@ -73,6 +78,7 @@ INSERT INTO plugin_methods (plugin_class, plugin_method) values
      ('Koha::Plugin::Fi::KohaSuomi::OverdueTool', 'rootdir'),
      ('Koha::Plugin::Fi::KohaSuomi::OverdueTool', 'search_path'),
      ('Koha::Plugin::Fi::KohaSuomi::OverdueTool', 'store_data'),
-     ('Koha::Plugin::Fi::KohaSuomi::OverdueTool', 'updir');
+     ('Koha::Plugin::Fi::KohaSuomi::OverdueTool', 'updir'),
+     ('Koha::Plugin::Fi::KohaSuomi::OverdueTool', 'tool');
 END
 

61d13a858fa76a98bff28d4c766fd310b92cd3e7
Johanna Raisa <johanna.raisa@gmail.com>
2022-06-15 13:48:10 +0300

    Add fix to set development

--- set-development.sh
+++ set-development.sh
@@ -8,8 +8,8 @@ rm $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/OverdueTool.pm
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-ln -s "$SCRIPT_DIR/koha-plugin-overdue-tool/Koha/Plugin/Fi/KohaSuomi/OverdueTool" $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/OverdueTool
-ln -s "$SCRIPT_DIR/koha-plugin-overdue-tool/Koha/Plugin/Fi/KohaSuomi/OverdueTool.pm" $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/OverdueTool.pm
+ln -s "$SCRIPT_DIR/Koha/Plugin/Fi/KohaSuomi/OverdueTool" $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/OverdueTool
+ln -s "$SCRIPT_DIR/Koha/Plugin/Fi/KohaSuomi/OverdueTool.pm" $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/OverdueTool.pm
 
 perl $kohadir/misc/devel/install_plugins.pl
 

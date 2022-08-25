ff87425c1219b74f2e620547c97ffc8c32b73dce
Johanna Raisa <johanna.raisa@gmail.com>
2022-08-25 14:47:17 +0300

    Add xml check again

--- Koha/Plugin/Fi/KohaSuomi/OverdueTool/Controllers/InvoiceController.pm
+++ Koha/Plugin/Fi/KohaSuomi/OverdueTool/Controllers/InvoiceController.pm
@@ -163,8 +163,14 @@ sub set {
         }
 
         my $notice = C4::Letters::GetPreparedLetter(%params);
-
-        $notice->{content} =~ s/>\s+</></g; if $body->{message_transport_type} eq "finvoice"
+        if ($body->{message_transport_type} eq "finvoice") {
+            my $content = eval { XML::LibXML->new()->parse_string($notice->{content}); };
+            if ($@) {
+                return $c->render(status => 400, openapi => {error => 'Finvoice is not valid XML for '.$body->{branchcode}});
+            }
+            $content =~ s/>\s+</></g;
+            $notice->{content} = $content;
+        }
 
         my $message_id;
 

a41871f1c85da6f11fe7007db27f59e24799b492
Emmi Takkinen <emmi.takkinen@koha-suomi.fi>
2024-01-29 10:23:16 +0200

    Add cron symlink creation to set-development.sh

--- set-development.sh
+++ set-development.sh
@@ -11,5 +11,8 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ln -s "$SCRIPT_DIR/Koha/Plugin/Fi/KohaSuomi/OverdueTool" $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/OverdueTool
 ln -s "$SCRIPT_DIR/Koha/Plugin/Fi/KohaSuomi/OverdueTool.pm" $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/OverdueTool.pm
 
+rm $kohadir/misc/cronjobs/run_finvoice.pl
+ln -s $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/OverdueTool/cronjobs/run_finvoice.pl $kohadir/misc/cronjobs/run_finvoice.pl
+
 perl $kohadir/misc/devel/install_plugins.pl
 
